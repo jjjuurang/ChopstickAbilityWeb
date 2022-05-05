cd627441ebed6bc37c0ff653472991fa4f79d4bf
nakyoung98 <66313756+nakyoung98@users.noreply.github.com>
2022-05-05 22:15:50 +0900

    make main.py and success to open file

--- reference/hand-gesture-recognition-mediapipe-main/hand-gesture-recognition-mediapipe-main/main.py
+++ reference/hand-gesture-recognition-mediapipe-main/hand-gesture-recognition-mediapipe-main/main.py
@@ -4,6 +4,7 @@ import csv
 import copy
 import argparse
 import itertools
+import time
 from collections import Counter
 from collections import deque
 
@@ -14,10 +15,11 @@ import mediapipe as mp
 from utils import CvFpsCalc
 from model import KeyPointClassifier
 
+
 def get_args():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--device", type=int, default=0)
+    parser.add_argument("--device", type=int, default=0) #카메라 관련
     parser.add_argument("--width", help='cap width', type=int, default=960)
     parser.add_argument("--height", help='cap height', type=int, default=540)
 
@@ -26,7 +28,6 @@ def get_args():
                         help='min_detection_confidence',
                         type=float,
                         default=0.8)
-
     parser.add_argument("--min_tracking_confidence",
                         help='min_tracking_confidence',
                         type=int,
@@ -51,6 +52,7 @@ def main():
     min_tracking_confidence = args.min_tracking_confidence
 
     use_brect = True
+
     # Camera preparation ###############################################################
     cap = cv.VideoCapture(cap_device)
     cap.set(cv.CAP_PROP_FRAME_WIDTH, cap_width)
@@ -67,7 +69,8 @@ def main():
 
     keypoint_classifier = KeyPointClassifier()
 
-        # Read labels ###########################################################
+
+    # Read labels ###########################################################
     with open('model/keypoint_classifier/keypoint_classifier_label.csv',
               encoding='utf-8-sig') as f:
         keypoint_classifier_labels = csv.reader(f)
@@ -75,21 +78,25 @@ def main():
             row[0] for row in keypoint_classifier_labels
         ]
 
+
     # FPS Measurement ########################################################
     cvFpsCalc = CvFpsCalc(buffer_len=10)
+
+
+
+    #  ########################################################################
+    mode = 0 #기본모드
     
-    # Coordinate history #################################################################
-    history_length = 16
-    point_history = deque(maxlen=history_length)
-
-
-    mode = 0
-
-    checkMovement=[]
-
+    validate = []
+    begin = time.time()
+    
     while True:
         fps = cvFpsCalc.get()
-        
+
+        # Process Key (ESC: end) #################################################
+        key = cv.waitKey(10)
+
+        mode = select_mode(key, mode)
 
         # Camera capture #####################################################
         ret, image = cap.read()
@@ -100,35 +107,70 @@ def main():
 
         # Detection implementation #############################################################
         image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
+
         image.flags.writeable = False
-        # Get result from image (camera)
-        results = hands.process(image) 
+        results = hands.process(image) #손인식 결과의 객체를 얻어오는 함수
         image.flags.writeable = True
 
-            #  ####################################################################
+        #  ####################################################################
         if results.multi_hand_landmarks is not None:
             for hand_landmarks, handedness in zip(results.multi_hand_landmarks,
                                                   results.multi_handedness):
+                # Bounding box calculation
+                brect = calc_bounding_rect(debug_image, hand_landmarks)
                 # Landmark calculation
                 landmark_list = calc_landmark_list(debug_image, hand_landmarks)
 
                 # Conversion to relative coordinates / normalized coordinates
                 pre_processed_landmark_list = pre_process_landmark(
                     landmark_list)
-                pre_processed_point_history_list = pre_process_point_history(
-                    debug_image, point_history)
+
+
 
                 # Hand sign classification
                 hand_sign_id = keypoint_classifier(pre_processed_landmark_list)
-                if hand_sign_id == 2:  # Point gesture
-                    point_history.append(landmark_list[8])
-                else:
-                    point_history.append([0, 0])
 
-        else:
-            point_history.append([0, 0])
+                validate.append(hand_sign_id) #append hand_result index
+
+
+                
+                # Drawing part
+                debug_image = draw_bounding_rect(use_brect, debug_image, brect)
+
+                debug_image = draw_hand_classification(debug_image, keypoint_classifier_labels[hand_sign_id])
+
+
+                
+        debug_image = draw_info(debug_image, fps, mode)
+
+
+
+        # Screen reflection #############################################################
+        cv.imshow('Hand Gesture Recognition', debug_image)
 
     cap.release()
+    cv.destroyAllWindows()
+
+
+
+
+
+def calc_bounding_rect(image, landmarks):
+    image_width, image_height = image.shape[1], image.shape[0]
+
+    landmark_array = np.empty((0, 2), int)
+
+    for _, landmark in enumerate(landmarks.landmark):
+        landmark_x = min(int(landmark.x * image_width), image_width - 1)
+        landmark_y = min(int(landmark.y * image_height), image_height - 1)
+
+        landmark_point = [np.array((landmark_x, landmark_y))]
+
+        landmark_array = np.append(landmark_array, landmark_point, axis=0)
+
+    x, y, w, h = cv.boundingRect(landmark_array)
+
+    return [x, y, x + w, y + h]
 
 
 def calc_landmark_list(image, landmarks):
@@ -173,27 +215,66 @@ def pre_process_landmark(landmark_list):
 
     return temp_landmark_list
 
-def pre_process_point_history(image, point_history):
-    image_width, image_height = image.shape[1], image.shape[0]
 
-    temp_point_history = copy.deepcopy(point_history)
 
-    # Convert to relative coordinates
-    base_x, base_y = 0, 0
-    for index, point in enumerate(temp_point_history):
-        if index == 0:
-            base_x, base_y = point[0], point[1]
+def draw_bounding_rect(use_brect, image, brect):
+    if use_brect:
+        # Outer rectangle
+        cv.rectangle(image, (brect[0], brect[1]), (brect[2], brect[3]),
+                     (0, 0, 0), 1)
 
-        temp_point_history[index][0] = (temp_point_history[index][0] -
-                                        base_x) / image_width
-        temp_point_history[index][1] = (temp_point_history[index][1] -
-                                        base_y) / image_height
+    return image
 
-    # Convert to a one-dimensional list
-    temp_point_history = list(
-        itertools.chain.from_iterable(temp_point_history))
 
-    return temp_point_history
+
+
+def select_mode(key, mode):
+
+    if key == 48:  #0
+        mode = 0
+        
+    elif key == 49: #1
+        mode = 1
+        
+    elif key == 50: #2
+        mode = 2
+
+    elif key == 51: #3
+        mode = 3
+        
+    return mode
+
+
+def draw_hand_classification(image, hand_sign_id):
+
+    cv.putText(image, "YOUR STEP: " + hand_sign_id, (10, 120),
+                   cv.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 1,
+                   cv.LINE_AA)
+    
+    return image
+    
+
+def draw_info(image, fps, mode):
+
+    mode_string = ''
+    
+    if mode == 0:
+        mode_string = 'STEP 0'
+    elif mode == 1:
+        mode_string = 'STEP 1'
+    elif mode == 2:
+        mode_string = 'STEP 2'
+    elif mode == 3:
+        mode_string = 'STEP 3'
+    
+    cv.putText(image, "MODE:" + mode_string, (10, 90),
+                   cv.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 1,
+                   cv.LINE_AA)
+
+    return image
+
+
+
 
 
 
